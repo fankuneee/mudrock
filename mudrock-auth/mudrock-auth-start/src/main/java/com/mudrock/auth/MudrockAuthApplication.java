9507b9e14b876c5f1d47ed02e9e9a9154beb07b1
huzhenghui <huzhenghui@miluoinfo.com>
2023-07-28 17:32:43 +0800

    init

---
+++ mudrock-auth/mudrock-auth-start/src/main/java/com/mudrock/auth/MudrockAuthApplication.java
@@ -0,0 +1,18 @@
+package com.mudrock.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: MudrockAuthApplication.
+ * Description:
+ * date: 2023/7/28 16:30
+ *
+ * @author huzhenghui
+ */
+@SpringBootApplication
+public class MudrockAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MudrockAuthApplication.class, args);
+    }
+}
